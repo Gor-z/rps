4b882be3439040428a4596a498a02c78a6e9a778
Gor-z <211397625+Gor-z@users.noreply.github.com>
2025-06-14 11:48:32 -0300

    add playRound func

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let humanScore = 0
+let computerScore = 0
+
+
 const getComputerChoice = () => {
     const choice = Math.floor(Math.random() * 3);
     
@@ -14,3 +18,24 @@ const getHumanChoice = () => {
     let choice = prompt('Chooce: Rock, Paper or Scissors!')
     return choice.charAt(0).toUpperCase() + choice.slice(1).toLocaleLowerCase();
 }
+
+
+const playRound = (humanChoice, computerChoice) => {
+    if (humanChoice == computerChoice) {
+       console.log('Tie!')
+    }else if(humanChoice == 'Rock' && computerChoice == 'Scissors'
+          || humanChoice == 'Scissors' && computerChoice == 'Paper'
+          || humanChoice == 'Paper' && computerChoice == 'Rock'
+    ){
+        humanScore ++
+        console.log(`You win! ${humanChoice} beats ${computerChoice}`)
+    }else{
+        computerScore ++
+        console.log(`You Lose! ${computerChoice} beats ${humanChoice}`)
+    }
+}
+
+const humanSelection = getHumanChoice()
+const computerSelection = getComputerChoice()
+
+playRound(humanSelection, computerSelection)
